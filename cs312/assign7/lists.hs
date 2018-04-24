349a78d7024a2fbd46d2c363ce5c258ef7b4f9e4
Jeremy Elkayam <jeremyelkayam@gmail.com>
2018-04-23 18:47:43 -0700

    finished more of the assignment

--- cs312/assign7/lists.hs
+++ cs312/assign7/lists.hs
@@ -31,24 +31,34 @@ main = do
      print (null list)
 
      -- [5,4,3,2,1] (reverse list)
+     print (reverse list)
 
      -- [1,2] (first two elements)
+     print (init (init (init list)))
 
      -- [3,4,5] (last three)
+     print (tail (tail list))
 
      -- 1 (lowest value)
+     print (minimum list)
 
      -- 5 (greatest value)
+     print (maximum list)
 
      -- 15 (sum of all values)
+     print (sum list)
 
      -- 120 (product of all values)
+     print (product list)
 
      -- [1,4,9,16,25] (each element squared)
+     print (maphead square list)
 
      -- False (check if every element is even)
+     print (all even list)
 
      -- True (check if at least one element is odd)
+     print (any odd list)
 
      -- [8,1,2,3,4,5] (concatenate 8 to front of list)
 
