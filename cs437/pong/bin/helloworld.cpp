b7f17df82d6baba7aef48bb79b80e018c947218e
Jeremy Elkayam <jeremyelkayam@gmail.com>
2018-09-09 12:19:18 -0400

    improved documentation in helloworld

--- cs437/pong/bin/helloworld.cpp
+++ cs437/pong/bin/helloworld.cpp
@@ -3,7 +3,7 @@
 int main(int argc, char** argv)
 {
   // create main window
-  sf::RenderWindow App(sf::VideoMode(800,600,32), "Hello World - SFML",sf::Style::Titlebar | sf::Style::Close);
+  sf::RenderWindow App(sf::VideoMode(800,600,32), "Hello World - SFML",sf::Style::Titlebar | sf::Style::Close); //enable titlebar & close options
   int alternator=0;
   // start main loop
   while(App.isOpen())
