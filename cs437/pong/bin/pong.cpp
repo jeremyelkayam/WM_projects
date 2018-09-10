3e357ff71511251789113425d2cf6c6025a6ab24
Jeremy Elkayam <jeremyelkayam@gmail.com>
2018-09-09 23:54:29 -0400

    add pong executable

---
+++ cs437/pong/bin/pong.cpp
@@ -0,0 +1,31 @@
+#include <SFML/Graphics.hpp>
+
+int main(int argc, char** argv)
+{
+  // create main window
+  sf::RenderWindow App(sf::VideoMode(800,600,32), "Pong (working title)",sf::Style::Titlebar | sf::Style::Close); //enable titlebar & close options
+  int alternator=0;
+  // start main loop
+  while(App.isOpen())
+  {
+    // process events
+    sf::Event Event;
+    while(App.pollEvent(Event))
+    {
+      // Exit
+      if(Event.type == sf::Event::Closed)
+        App.close();
+    }
+
+    // clear screen and fill with blue
+    
+    App.clear(sf::Color::Blue);
+    
+    
+    // display
+    App.display();
+  }
+
+  // Done.
+  return 0;
+}
