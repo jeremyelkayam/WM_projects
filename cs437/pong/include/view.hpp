1ae7a2a31806020b37d5c894e5a206ef1ed90632
Jeremy Elkayam <jeremyelkayam@gmail.com>
2018-09-10 15:01:25 -0400

    add gitignore, add view framework

---
+++ cs437/pong/include/view.hpp
@@ -0,0 +1,19 @@
+#ifndef VIEW_HPP
+#define VIEW_HPP
+
+
+class View
+{
+private:
+  Ball ball;
+
+public:
+  View(Ball ball);
+
+  void get_ball_xcor();
+  void get_ball_ycor();
+  
+  
+}
+
+#endif
