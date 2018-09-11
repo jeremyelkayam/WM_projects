c2223e1b32b6edc9979d96f8629949ca3a02fdb3
Jeremy Elkayam <jeremyelkayam@gmail.com>
2018-09-10 23:54:29 -0400

    Renamed View and corresponding files to renderer, remembering to add files

---
+++ cs437/pong/include/renderer.hpp
@@ -0,0 +1,30 @@
+/*
+  renderer.hpp
+  Purpose: Consolidate all rendering operations into a single class, with one
+           function that executes with each iteration of the main loop.
+
+  @author Jeremy Elkayam
+ */
+#ifndef RENDERER_HPP
+#define RENDERER_HPP
+
+#include <SFML/Graphics.hpp>
+#include "game.hpp"
+
+class Renderer
+{
+private:
+  //A reference to the properties of the game being played.
+  Game *game;
+  sf::RenderWindow *window;
+
+  void draw_ball();
+
+public:
+  Renderer(Game *game,sf::RenderWindow *window);
+  
+  void update();
+  
+};
+
+#endif
---
+++ cs437/pong/src/renderer.cpp
@@ -0,0 +1,35 @@
+/*
+  view.cpp
+  Purpose: 
+
+  @author Jeremy Elkayam
+ */
+#include <SFML/Graphics.hpp>
+#include "renderer.hpp"
+
+Renderer::Renderer(Game *game, sf::RenderWindow *window)
+{
+  this->game=game;
+  this->window=window;
+}
+
+void Renderer::update()
+{
+  //clear window to prepare for rendering
+  window->clear(sf::Color::Black);
+  
+  draw_ball();
+
+  window->display();
+  
+}
+
+
+void Renderer::draw_ball()
+{
+  sf::CircleShape ball(5);
+
+  ball.setPosition(game->get_ball()->get_xcor(),game->get_ball()->get_ycor());
+
+  window->draw(ball);
+}
