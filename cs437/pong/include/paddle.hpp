d734e558d42be44ec29e6d72a5076aa660896f6b
Jeremy Elkayam <jeremyelkayam@gmail.com>
2018-09-13 09:36:41 -0400

    added move function in Paddle class

--- cs437/pong/include/paddle.hpp
+++ cs437/pong/include/paddle.hpp
@@ -7,19 +7,23 @@
 #ifndef GAME_HPP
 #define GAME_HPP
 
+
 class Paddle
 {
 private:
   double speed,accel,ycor;
 
 public:
+  enum class Direction { Up, Down };
+  
   Paddle(double speed, double accel, double ycor);
 
   void set_speed(double speed){this->speed=speed;}
   void set_accel(double accel){this->accel=accel;}
   void set_ycor(double ycor){this->ycor=ycor;}
 
-  void move(double ms_elapsed);
+  void move(double micros_elapsed, Direction dir);
+
   
 };
 
--- cs437/pong/src/paddle.cpp
+++ cs437/pong/src/paddle.cpp
@@ -12,3 +12,21 @@ Paddle::Paddle(double speed, double accel, double ycor)
   set_accel(accel);
   set_ycor(ycor);
 }
+
+void Paddle::move(double micros_elapsed, Direction dir)
+{
+  int multiplier;
+  if(dir==Direction::Up)
+    {
+      multiplier=-1;
+    }
+  else
+    {
+      multiplier=1;
+    }
+  
+  
+  double delta_y=this->speed*micros_elapsed*multiplier;
+
+  this->ycor+=delta_y;
+}
