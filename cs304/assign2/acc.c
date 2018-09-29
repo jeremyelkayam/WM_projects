5f68026000e159ffcda07f6aee7086a13f06b0e6
Jeremy Elkayam <jeremyelkayam@gmail.com>
2018-09-29 19:23:50 -0400

    created okay, placeholder print_menu function

--- cs304/assign2/acc.c
+++ cs304/assign2/acc.c
@@ -1,5 +1,6 @@
 // @author Jeremy Elkayam
 #include <stdio.h>
+#include <ctype.h>
 
 //read in numeric value in mode, return value
 short get_operand(char mode);
@@ -8,11 +9,17 @@ void print_acc(short acc);
 //print menu and get option
 char print_menu(void);
 
+int value=0;
 
 //main menu loop; execute option or call appropriate function
 int main(void)
 {
+  char result;
+  do{
+    result=print_menu();
+    printf("result: %c\n",result);
 
+  }while(result!='q');
 
   return 0;
 }
@@ -30,5 +37,24 @@ void print_acc(short acc)
 
 char print_menu(void)
 {
-  //TODO: implement
+  printf("****************************************\n* Accumulator:                         *\n*   Hex     :  %x,                    *\n*   Octal   :  %o                  *\n*   Decimal :  %d                       *\n****************************************\nPlease select one of the following options: \n\nO  Octal Mode \nH  Hexidecimal Mode \nD  Decimal Mode \n\nC  Clear Accumulator \nS  Set Accumulator \nQ  Quit \n\nOption: ",value,value,value);
+
+  char output;
+  
+  scanf("%c",&output);
+  
+  output=tolower(output);
+
+  /*
+  while(output!='o' &&
+	output!='h' &&
+	output!='d' &&
+	output!='c' &&
+	output!='s' &&
+	output!='q'){
+    printf("No valid input received.\nOption: ");
+    scanf("%c",&output);
+  }
+  */
+  return output;
 }
