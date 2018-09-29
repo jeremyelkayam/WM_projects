a63eab14bac4e7392d719319c07ee0858de04e5a
Jeremy Elkayam <jeremyelkayam@gmail.com>
2018-09-29 19:34:55 -0400

    implemented basic features

--- cs304/assign2/acc.c
+++ cs304/assign2/acc.c
@@ -9,14 +9,42 @@ void print_acc(short acc);
 //print menu and get option
 char print_menu(void);
 
-int value=0;
 
 //main menu loop; execute option or call appropriate function
 int main(void)
 {
+  short stored_value=0;
+  char mode='d';
   char result;
   do{
+    print_acc(stored_value);
     result=print_menu();
+
+    if(result=='d')
+      {
+	mode=result;
+	printf("Mode is Decimal.\n");
+      }
+    else if(result=='h')
+      {
+	mode='x';
+	printf("Mode is Hexadecimal.\n");
+      }
+    else if(result=='o')
+      {
+	mode=result;
+	printf("Mode is Octal.\n");
+      }
+    else if(result=='c')
+      {
+	stored_value=0;
+	printf("Cleared.\n");
+      }
+    else if(result=='s')
+      {
+	stored_value=get_operand(mode);
+      }
+    
     printf("result: %c\n",result);
 
   }while(result!='q');
@@ -26,19 +54,25 @@ int main(void)
 
 short get_operand(char mode)
 {
-  // TODO: implement
-  return 0;
+  short result;
+  
+  char asking_for[2];
+  asking_for[0]='%';
+  asking_for[1]=mode;
+
+  scanf(asking_for,&result);
+  
+  return result;
 }
 
 void print_acc(short acc)
 {
-  //TODO: implement
+  printf("****************************************\n* Accumulator:                         *\n*   Hex     :  %X                    *\n*   Octal   :  %o                  *\n*   Decimal :  %d                       *\n****************************************\nPlease select one of the following options: \n\nO  Octal Mode \nH  Hexidecimal Mode \nD  Decimal Mode \n\nC  Clear Accumulator \nS  Set Accumulator \nQ  Quit \n\n",acc,acc,acc);
 }
 
 char print_menu(void)
 {
-  printf("****************************************\n* Accumulator:                         *\n*   Hex     :  %x,                    *\n*   Octal   :  %o                  *\n*   Decimal :  %d                       *\n****************************************\nPlease select one of the following options: \n\nO  Octal Mode \nH  Hexidecimal Mode \nD  Decimal Mode \n\nC  Clear Accumulator \nS  Set Accumulator \nQ  Quit \n\nOption: ",value,value,value);
-
+  printf("Option: ");
   char output;
   
   scanf("%c",&output);
