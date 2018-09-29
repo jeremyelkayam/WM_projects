7cf457e4841daf5b2b635cba1c12bd4ffc913b56
Jeremy Elkayam <jeremyelkayam@gmail.com>
2018-09-29 18:52:01 -0400

    add skeleton code for 304 assign 2

---
+++ cs304/assign2/acc.c
@@ -0,0 +1,34 @@
+// @author Jeremy Elkayam
+#include <stdio.h>
+
+//read in numeric value in mode, return value
+short get_operand(char mode);
+//print out accumulator values
+void print_acc(short acc);
+//print menu and get option
+char print_menu(void);
+
+
+//main menu loop; execute option or call appropriate function
+int main(void)
+{
+
+
+  return 0;
+}
+
+short get_operand(char mode)
+{
+  // TODO: implement
+  return 0;
+}
+
+void print_acc(short acc)
+{
+  //TODO: implement
+}
+
+char print_menu(void)
+{
+  //TODO: implement
+}
