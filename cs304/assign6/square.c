da6f45c06fb266519b652bcbaa990af644aa7e44
Jeremy Elkayam <jeremyelkayam@gmail.com>
2018-11-13 11:42:28 -0500

    add c code

--- cs304/assign6/square.c
+++ cs304/assign6/square.c
@@ -1,17 +1,37 @@
+#include <stdio.h>
+
 int squareadd(int i){
   int result=0;
   int z;
   for(z=0;z<i;z++){
-    result+=z;
+    result+=i;
   }
   return result;
 }
 
 int squarebit(int i){
-  return i*i; //todo actually do it
+  int sum = 0;
+  int mult = i;
+  int bit = 0;
+
+  while(bit<=i){
+    if(i & (1<<bit)){
+      sum+=mult;
+    }
+    bit+=1;
+    mult+=mult;
+  }
+  
+  return sum; 
 }
 
 int main(){
+  int i;
+  for(i=1;i<=20;i++){
+    int sqa=squareadd(i);
+    int sqb=squarebit(i);
 
+    printf("%3d%3X%4X%4X\n",i,i,sqa,sqb);
+  }
   return 0;
 }
